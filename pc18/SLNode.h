efb4ec0726d2c98ddbb62b3f0f94b6a65e6122a9
jslaton001 <jslaton001@gmail.com>
2014-04-09 10:13:53 -0700

    Update SLNode.h

--- pc18/SLNode.h
+++ pc18/SLNode.h
@@ -7,11 +7,43 @@ class SLNode
     int mContents;
     SLNode* mNextNode;
 public:
+    /*
+     * Constructor for SLNode. Simply initializes the member variables.
+     */
     SLNode();
+    
+    /*
+     * Overloaded constructor for SLNode.
+     * @param contents sets mContents to contents
+     */
     SLNode(int contents);
+    
+    /*
+     * Effectively does nothing. It does set mNextNode to NULL.
+     */
     ~SLNode();
+    
+    /*
+     * Sets the value of contents.
+     * @param contents sets mContents to contents
+     */
     void setContents(int contents);
+    
+    /*
+     * Sets the value of nextNode.
+     * @param nextNode sets mNextNode to nextNode
+     */
     void setNextNode(SLNode* nextNode);
+    
+    /*
+     * Returns mContents.
+     * @return mContents
+     */
     int getContents();
+    
+    /*
+     * Returns mNextNode.
+     * @return mNextNode
+     */
     SLNode* getNextNode();
 };
