9ea074e707aa32b054dfabdfbc957e13f09d2c35
jslaton001 <jslaton001@gmail.com>
2014-04-09 10:14:23 -0700

    Update SList.h

--- pc18/SList.h
+++ pc18/SList.h
@@ -9,16 +9,46 @@ using std::stringstream;
 
 class SList
 {
-        SLNode* mHead;
-        unsigned int mNumNodes;
-    public:
-        SList();
-        virtual ~SList();
-        void insertHead(int value);
-        void removeHead();
-        void clear();
-        unsigned int getSize();
-        string toString();
-    protected:
-    private:
+    SLNode* mHead;
+    unsigned int mNumNodes;
+public:
+    /*
+     * Constructor for SList. Simply initializes the member variables.
+     */
+    SList();
+    
+    /*
+     * Deletes the list.
+     */
+    virtual ~SList();
+    
+    /*
+     * Inserts a new node in the list at the head.
+     * @param value the data to be stored in the list\
+     */
+    void insertHead(int value);
+    
+    /*
+     * Remove the node at the head (if it exists).
+     */
+    void removeHead();
+    
+    /*
+     * Deletes the contents of the list and sets mHead to NULL;
+     */
+    void clear();
+    
+    /*
+     * Returns the number of nodes in the list.
+     * @return mNumNodes
+     */
+    unsigned int getSize();
+    
+    /*
+     * Returns the contents of the list in the format "NUM1,NUM2,...,LASTNUM".
+     * @return the list contents, or "" if mHead is NULL
+     */
+    string toString();
+protected:
+private:
 };
